e2937793aa4755076420fed4949edd941fd02c35
majazocom <maja.gedda@zocom.se>
2022-03-30 14:34:49 +0200

    last changes

--- src/views/AddPlant.jsx
+++ src/views/AddPlant.jsx
@@ -1,34 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { PlantsContext } from '../App';
-import Button from '../Components/Button'
+import Button from '../Components/Button';
 
 export default function AddPlant() {
-    const {plants, setPlants} = useContext(PlantsContext);
-    const id = plants.length;
+    const { plants, setPlants } = useContext(PlantsContext);
 
     const [newPlant, setNewPlant] = useState({
-        id: id,
+        id: plants.length,
         name: "",
         sunCravings: 0
     });
 
-    useEffect(() => {
-        console.log(plants);
-    },[plants]);
-
     function handleAddClick() {
-        setNewPlant({...newPlant, id: id});
-        setPlants(plants => [...plants, newPlant])
+        const id = plants.length;
+        setNewPlant({ ...newPlant, id: id });
+        //här hinner vi inte uppdatera newPlant's id innan vi vill uppdatera plant
+        setPlants(plants => [...plants, newPlant]);
     }
 
-  return (
-    <main>
-        <h1>Add plant:</h1>
-        <label htmlFor='name'>Plant name</label>
-        <input onChange={(e) => {setNewPlant({...newPlant, name: e.target.value})}} id="name" type="text" placeholder='Plant name'/>
-        <label htmlFor="suncravings">Sun cravings:</label>
-        <input onChange={(e) => {setNewPlant({...newPlant, sunCravings: parseInt(e.target.value)})}} id="suncravings" type="number" placeholder='1'/>
-        <Button action={handleAddClick} title="Add plant" />
-    </main>
-  )
+    return (
+        <main>
+            <h1>Add plant:</h1>
+            <label htmlFor='name'>Plant name</label>
+            <input onChange={(e) => { setNewPlant({ ...newPlant, name: e.target.value }) }} id="name" type="text" placeholder='Plant name' />
+            <label htmlFor="suncravings">Sun cravings:</label>
+            <input onChange={(e) => { setNewPlant({ ...newPlant, sunCravings: parseInt(e.target.value) }) }} id="suncravings" type="number" placeholder='1' />
+            <Button action={handleAddClick} title="Add plant" />
+        </main>
+    )
 }
